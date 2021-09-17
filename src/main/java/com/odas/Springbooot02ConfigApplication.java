720ebd61cb82df026dcfc4d38c4a850ebdabfeda
odas <zwtao106@gmail.com>
2021-09-18 02:27:29 +1000

    add the whole project

---
+++ src/main/java/com/odas/Springbooot02ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.odas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbooot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbooot02ConfigApplication.class, args);
+    }
+
+}
